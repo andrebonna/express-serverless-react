56cd0381249d2dbace18a53dc3fe88e509263d23
André Bonna <andre.bonna@pminova.com>
2017-10-06 19:03:51 -0300

    Remove useless categories from contants

--- src/Serverless/commons/constants.js
+++ src/Serverless/commons/constants.js
@@ -1,18 +1,5 @@
 export default {
     metas: {
         keywords: 'photos, nature, urban, photography, fotografia'
-    },
-    categories: [{
-        name: 'All',
-        href: 'categories'
-    },{
-        name: 'Urban',
-        href: 'categories?filter=urban'
-    },{
-        name: 'Nature',
-        href: 'categories?filter=nature'
-    },{
-        name: 'Animals',
-        href: 'categories?filter=animals'
-    }]
+    }
 };
